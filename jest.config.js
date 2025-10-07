02bc3d2e01235f69f085265ebd3e6f3fb2d65b7c
Ubuntu <azureuser@backstage-vm.nvmg0iiz0vielcbp1nagaaah4c.bx.internal.cloudapp.net>
2025-10-07 07:21:51 +0000

    FINAL FIX: Clean Jest configuration
    - Completely recreate jest.config.js from scratch
    - Remove all corrupted multiple module.exports statements
    - Use simple, clean Jest configuration
    - This should resolve the syntax error once and for all

--- jest.config.js
+++ jest.config.js
@@ -1,44 +1,11 @@
-module.exports = {module.exports = {module.exports = {
-
+module.exports = {
   testEnvironment: 'node',
-
-  testMatch: ['**/tests/**/*.test.js'],  testEnvironment: 'node',  // Only test JavaScript files in our tests directory
-
-  collectCoverageFrom: [
-
-    'src/**/*.js',  testMatch: ['**/tests/**/*.test.js'],  testMatch: ['<rootDir>/tests/**/*.test.js'],
-
-    '!src/index.js'
-
-  ],  collectCoverageFrom: [  
-
-  coverageDirectory: 'coverage',
-
-  coverageReporters: ['text', 'lcov', 'html'],    'src/**/*.js',  // Ignore Backstage TypeScript tests
-
-  verbose: true
-
-};    '!src/index.js'  testPathIgnorePatterns: [
-
-  ],    '<rootDir>/node_modules/',
-
-  coverageDirectory: 'coverage',    '<rootDir>/packages/',
-
-  coverageReporters: ['text', 'lcov', 'html'],    '<rootDir>/plugins/'
-
-  verbose: true  ],
-
-};  
-  // Test environment
-  testEnvironment: 'node',
-  
-  // Coverage settings
+  testMatch: ['**/tests/**/*.test.js'],
   collectCoverageFrom: [
     'src/**/*.js',
-    '!src/config/**',
-    '!**/node_modules/**'
+    '!src/index.js'
   ],
-  
-  // Timeout settings
-  testTimeout: 10000
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  verbose: true
 };
